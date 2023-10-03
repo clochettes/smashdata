8fca359f1581a0b93d2d6f17adeedeeca536f317
Yanis Saadaoui <146101928+clochettes@users.noreply.github.com>
2023-10-03 09:40:28 +0000

    Update

---
+++ web-ult-data/components/Header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+  const arv = "aa";
+  return (
+    <>
+      <a>O</a>
+      <ul>aa</ul>
+    </>
+  );
+}
---
+++ web-ult-data/pages/index.js
@@ -0,0 +1,13 @@
+import Header from "../components/Header.js";
+
+export default function Home() {
+  return (
+    <>
+      <head>
+        <title>Salut</title>
+      </head>
+      <Header />
+      <a></a>
+    </>
+  );
+}
